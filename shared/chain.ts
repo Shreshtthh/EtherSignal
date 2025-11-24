ae4fa4fe3e8e4c1c85d15c8e50ec8beeccc1cd25
Shreshtthh <shreshth013@gmail.com>
2025-11-25 02:46:45 +0530

    Add shared chain.ts

---
+++ shared/chain.ts
@@ -0,0 +1,29 @@
+import { defineChain } from 'viem'
+
+export const somniaTestnet = defineChain({
+  id: 50312,
+  name: 'Somnia Testnet',
+  network: 'somnia-testnet',
+  nativeCurrency: {
+    name: 'STT',
+    symbol: 'STT',
+    decimals: 18
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://dream-rpc.somnia.network'],
+      webSocket: ['wss://dream-rpc.somnia.network/ws']
+    },
+    public: {
+      http: ['https://dream-rpc.somnia.network'],
+      webSocket: ['wss://dream-rpc.somnia.network/ws']
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: 'Somnia Explorer',
+      url: 'https://somnia-devnet.socialscan.io'
+    }
+  },
+  testnet: true
+})
